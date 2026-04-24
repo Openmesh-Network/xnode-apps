3a6dfccf130944fcb34ec69dd3a20cf5beb46e87
Plopmenz <plopmenz@gmail.com>
2026-04-24 11:04:44 +0200

    add hermes-agent

---
+++ hermes-agent/flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    hermes-agent.url = "github:NousResearch/hermes-agent";
+    nixpkgs.follows = "hermes-agent/nixpkgs";
+    xnodeos = {
+      url = "github:Openmesh-Network/xnodeos/WIP";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: {
+    nixosModules = {
+      default =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            inputs.hermes-agent.nixosModules.default
+            inputs.xnodeos.nixosModules.app
+          ];
+
+          config = {
+            xnode.manager.cache = [
+              {
+                location = "https://cache.garnix.io";
+                public-keys = [ "CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+              }
+            ];
+
+            services.hermes-agent.enable = true;
+          };
+        };
+    };
+  };
+}
