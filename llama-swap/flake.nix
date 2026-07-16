48298caeaafc231cea57266fdffb26e02271b785
Plopmenz <plopmenz@gmail.com>
2026-07-16 13:37:26 +0200

    add llama-swap

---
+++ llama-swap/flake.nix
@@ -0,0 +1,61 @@
+{
+  inputs = {
+    xnodeos.url = "github:Openmesh-Network/xnodeos/v2";
+    nixpkgs.follows = "xnodeos/nixpkgs";
+  };
+
+  outputs = inputs: {
+    nixosModules = {
+      default =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            inputs.xnodeos.nixosModules.app
+          ];
+
+          config =
+            let
+              domain =
+                if (builtins.pathExists "${args.config.xnode.xnode-config}/domain") then
+                  builtins.readFile "${args.config.xnode.xnode-config}/domain"
+                else
+                  "";
+              owner =
+                if (builtins.pathExists "${args.config.xnode.xnode-config}/owner") then
+                  builtins.readFile "${args.config.xnode.xnode-config}/owner"
+                else
+                  "";
+            in
+            {
+              services.llama-swap.enable = true;
+              services.llama-swap.listenAddress = "127.0.0.1";
+
+              xnode.reverse-proxy.https = args.lib.mkIf (domain != "") {
+                ${domain}."/".locations = [
+                  {
+                    domain = args.config.services.llama-swap.listenAddress;
+                    port = args.config.services.llama-swap.port;
+                  }
+                ];
+              };
+
+              services.xnode-auth.domains = args.lib.mkIf (domain != "" && owner != "") {
+                ${domain} = {
+                  accessList = {
+                    users = {
+                      ${owner} = {
+                        roles = [ "owner" ];
+                      };
+                    };
+                    roles = {
+                      "owner" = { };
+                    };
+                  };
+                  paths = builtins.attrNames args.config.xnode.reverse-proxy.https.${domain};
+                };
+              };
+            };
+        };
+    };
+  };
+}
